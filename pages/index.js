c0b5821a8be73303920f218870911b1ae8f34275
timothy <timothy.christian10@gmail.com>
2024-01-24 05:56:07 +0100

    still working on the landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,56 @@
+import Image from "next/image";
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Transfroming ideas <br />
+            Into <span className="text-accent">Digital Reality</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            I am a software developer, specialized in web development. I have
+            over a year of experience in different web programming languages
+          </motion.p>
+          {/* btn */}
+          {/* <div className="flex justify-center xl:hidden relative">
+            <ProjectBtn />
+          </div> */}
+          <motion.div
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex "
+          >
+            <ProjectBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
