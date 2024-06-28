d7b09ef3e2e5ca793afd19d646c79b3620df11e6
timothy <timothy.christian10@gmail.com>
2024-06-28 16:24:01 +0100

    updated testimonial for john edem

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -12,7 +12,7 @@ const testimonialData = [
     name: "Jane Edem",
     position: "Customer",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+      "Timothy has been so helpful in building and maintaining my website ",
   },
   {
     image: "/t-avt-3.png",
