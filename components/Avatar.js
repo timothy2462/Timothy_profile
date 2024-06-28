f2db7e5c4207025464ee7fd86bcdf0f4a0853937
timothy <timothy.christian10@gmail.com>
2024-06-28 16:36:14 +0100

    removed console logs from the avatar section

--- components/Avatar.js
+++ components/Avatar.js
@@ -15,28 +15,3 @@ const Avatar = () => {
 };
 
 export default Avatar;
-
-// import Image from "next/image";
-
-// const Avatar = () => {
-//   return (
-//     <div
-//       style={{
-//         display: "block",
-//         "@media screen and (max-width: 599px)": {
-//           display: "none",
-//         },
-//       }}
-//     >
-//       <Image
-//         src={"/avatar.png"}
-//         width={500}
-//         height={500}
-//         alt=""
-//         className=" w-full h-full translate-x-0  "
-//       />
-//     </div>
-//   );
-// };
-
-// export default Avatar;
